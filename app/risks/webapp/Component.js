785480de3c359267188d2dc337a8c742f1cc2077
mohithmbansal <mohithmbansal@gmail.com>
2022-01-25 00:54:55 +0000

    Push Project into Git Hub

---
+++ app/risks/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("Riskmanagement.risks.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
